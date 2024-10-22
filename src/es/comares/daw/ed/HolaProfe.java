7d489501dfd4e8c1fccf1651097ce19ded73c6ce
Maria Araceli Pacheco <aracelipacheco@gmail.com>
2024-10-22 12:44:41 +0200

    Commit Araceli

--- src/es/comares/daw/ed/HolaProfe.java
+++ src/es/comares/daw/ed/HolaProfe.java
@@ -5,7 +5,7 @@ public class HolaProfe {
 	public static void main(String[] args) {
 		
 		System.out.println("Hola, mi nombre es Jesús Meléndez");
-
+		System.out.println("Hola, mi nombre es Maria Araceli Pacheco");
 	}
 
 }
