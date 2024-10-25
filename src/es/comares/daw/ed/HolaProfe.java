a141f3f23c3459310512e3234c2543b43c204f8b
Jesus Melendez <jemeo29@gmail.com>
2024-10-25 08:44:37 +0200

    Cambio commit and push

--- src/es/comares/daw/ed/HolaProfe.java
+++ src/es/comares/daw/ed/HolaProfe.java
@@ -8,6 +8,9 @@ public class HolaProfe {
 		System.out.println("Hola, mi nombre es Luis Sánchez");
 		System.out.println("Hola, mi nombre es Maria Araceli Pacheco");
 		System.out.println("Buenas, soy Manolo. Ahora debéis obtener los cambios desde Eclipse. No olividéis documentarlo con capturas de pantalla ");
+		
+		System.out.println("Este va a ser el cambio!");
+		
 	}
 
 }
